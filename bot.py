631b7869bde9576024dd37c9ab9cba98c8e3ee02
Unki24 <maxim24112004@gmail.com>
2025-06-02 00:25:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -442,7 +442,8 @@ def index():
 if __name__ == "__main__":
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(handle_callback))
- app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+ 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 def setup_webhook():
     import asyncio
